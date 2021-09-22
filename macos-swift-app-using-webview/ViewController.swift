3d6015f829a0ad5b51639f672c8a19e00d3bb119
Tommy <tommy@tommyjepsen.com>
2021-09-22 16:26:52 +0200

    Using a Webview in Native MacOS app

--- macos-swift-app-using-webview/ViewController.swift
+++ macos-swift-app-using-webview/ViewController.swift
@@ -1,26 +1,33 @@
 //
 //  ViewController.swift
-//  macos-swift-app-using-webview
+//  krimv2
 //
 //  Created by Tommy Jepsen on 22/09/2021.
 //
-
+import WebKit
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, WKUIDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+      var webView: WKWebView!
+    
+      override func loadView() {
+          super.loadView()
+          let webConfiguration = WKWebViewConfiguration()
+            webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 1440, height: 790), configuration: webConfiguration)
 
-        // Do any additional setup after loading the view.
-    }
+          webView.uiDelegate = self
+          view = webView
+      }
+    
+      override func viewDidLoad() {
+          super.viewDidLoad()
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
+          var myURL: URL!
+          myURL = URL(string: "https://digitale.dk/")
+          let myRequest = URLRequest(url: myURL!)
+          webView.load(myRequest)
+      }
 }
 
