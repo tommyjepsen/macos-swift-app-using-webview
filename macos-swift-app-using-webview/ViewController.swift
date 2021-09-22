931e5950a6a614e01fea79cdfb7253def87293cc
Tommy <tommy@tommyjepsen.com>
2021-09-22 16:25:27 +0200

    Initial Commit

---
+++ macos-swift-app-using-webview/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macos-swift-app-using-webview
+//
+//  Created by Tommy Jepsen on 22/09/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
